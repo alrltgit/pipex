24c24fe55023884c04de73a6f9b0553dc4788170
Alina Raltsevich <alraltse@c3r2s3.42warsaw.pl>
2025-04-03 17:46:59 +0200

    Update

--- Makefile
+++ Makefile
@@ -3,10 +3,10 @@
 #                                                         :::      ::::::::    #
 #    Makefile                                           :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
-#    By: apple <apple@student.42.fr>                +#+  +:+       +#+         #
+#    By: alraltse <alraltse@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2025/03/29 17:11:47 by apple             #+#    #+#              #
-#    Updated: 2025/04/01 16:36:43 by apple            ###   ########.fr        #
+#    Updated: 2025/04/03 17:44:51 by alraltse         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -32,14 +32,14 @@ LIBFT_PATH = libft
 LIBFT_NAME = libft.a
 
 $(NAME): $(OBJS)
-	@make -C $(LIBFT_PATH) --no-print-directory
+	@make -s -C $(LIBFT_PATH)
 	@$(CC) $(CFLAGS) $(OBJS) -g -L$(LIBFT_PATH) -lft -o $(NAME)
 	@echo "Compilation completed successfully."
 
 all: $(NAME)
 
 clean:
-	@make clean -C $(LIBFT_PATH) --no-print-directory
+	@make clean -s -C $(LIBFT_PATH) --no-print-directory
 	@$(RM) $(OBJS)
 	@echo "Object files removed."
 
--- create_pipe.c
+++ create_pipe.c
@@ -6,7 +6,7 @@
 /*   By: alraltse <alraltse@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/31 17:49:04 by apple             #+#    #+#             */
-/*   Updated: 2025/04/03 15:58:04 by alraltse         ###   ########.fr       */
+/*   Updated: 2025/04/03 17:40:01 by alraltse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,7 @@ void create_pipe(t_cmd *c, char **argv)
     int pipe_fd_1;
     int pipe_fd_2;
     int pipe_fd[2];
+    int prev_pipe;
     char *args_1[] = {c->cmd_1, "-l", NULL};
     char *args_2[] = {c->cmd_2, "-l", NULL};
     char *envp[] = {NULL};
@@ -40,6 +41,7 @@ void create_pipe(t_cmd *c, char **argv)
     pid_t pid_2;
 
     (void)argv;
+    prev_pipe = STDIN_FILENO;
     pipe_fd_1 = open("infile", O_RDONLY);
     if (pipe_fd_1 < 0)
     {
@@ -47,7 +49,8 @@ void create_pipe(t_cmd *c, char **argv)
         exit(EXIT_FAILURE);
     }
     dup2(pipe_fd_1, STDIN_FILENO);
-    pipe_fd_2 = open("./outfile", O_RDWR | O_CREAT | O_TRUNC, 0644);
+    pipe_fd_2 = open("outfile", O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (pipe_fd_2 < 0)
     {
         perror("Error opening pipe_fd_2.\n");
         exit(EXIT_FAILURE);
@@ -66,7 +69,7 @@ void create_pipe(t_cmd *c, char **argv)
     }
     else if (pid_1 == 0) // child process 1
     {
-        dup2(pipe_fd[0], STDIN_FILENO);
+        dup2(prev_pipe, STDIN_FILENO);
         execve(c->cmd_1, args_1, envp);
         close(pipe_fd[0]);
         close(pipe_fd[1]);
@@ -79,12 +82,12 @@ void create_pipe(t_cmd *c, char **argv)
     }
     else if (pid_2 == 0) // child process 2
     {
-        dup2(pipe_fd[0], STDIN_FILENO);
+        dup2(pipe_fd[1], STDOUT_FILENO);
         execve(c->cmd_2, args_2, envp);
         close(pipe_fd[0]);
         close(pipe_fd[1]);
     }
-    close(pipe_fd[0]);
+    prev_pipe = pipe_fd[0];
     close(pipe_fd[1]);
     while (wait(NULL) > 0);
 }
