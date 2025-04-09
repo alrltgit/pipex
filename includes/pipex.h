3ffed4c9f18415171938e7642b6fcd21517986e4
Alina <ralcevichalina70@gmail.com>
2025-04-09 18:15:09 +0200

    Update

--- includes/pipex.h
+++ includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: apple <apple@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/29 17:12:22 by apple             #+#    #+#             */
-/*   Updated: 2025/04/09 17:56:35 by apple            ###   ########.fr       */
+/*   Updated: 2025/04/09 18:14:38 by apple            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,7 @@ typedef struct s_cmd
 	char	*flag_2;
 	char	**cmds;
 	char	**args;
-	char 	***args_bonus;
+	char	***args_bonus;
 	int		cmd_count;
 }	t_cmd;
 
@@ -51,13 +51,12 @@ void	fd_is_open(int pipe_fd_2);
 int		cmd_1_is_valid(t_cmd *c, char **cmd_folders, char **argv);
 int		cmd_2_is_valid(t_cmd *c, char **cmd_folders, char **argv);
 int		do_commands_exist(t_cmd *c, char **argv, char **cmd_folders);
-void		create_pipe(t_cmd *c, char **argv);
+void	create_pipe(t_cmd *c, char **argv);
 char	**create_arr_for_execve(char *cmd, char *flag);
 void	find_flags(t_cmd *c, char **argv);
 char	*concat_several_flags(char *flag, char **argv_arr);
 char	*ft_arrconcat(char *path, char *str);
-int	find_size_of_arr(char **argv);
-
+int		find_size_of_arr(char **argv);
 char	**allocate_memory_bonus(t_cmd *c);
 void	add_cmds_to_arr(t_cmd *c, char **argv);
 void	add_args_to_arr(t_cmd *c, char **argv);
@@ -68,4 +67,5 @@ void	did_fork_fail(pid_t pid);
 void	free_arr(char **array);
 void	free_args(char ***args);
 void	free_program(t_cmd *c);
+
 #endif
